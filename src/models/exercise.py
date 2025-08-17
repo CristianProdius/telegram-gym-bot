bb8d85cecbaa30f56e78e4112670e2c1e9c79106
veacheslavv <144955981+veacheslavv@users.noreply.github.com>
2025-08-17 21:33:20 +0300

    Update exercise.py

---
+++ src/models/exercise.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Exercise(Base):
+    __tablename__ = "exercises"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(200), nullable=False)
+    category = Column(String(100))
+    primary_muscle = Column(String(100))
+
+    def __repr__(self):
+        return f"<Exercise(name={self.name}, category={self.category}, muscle={self.primary_muscle})>"
