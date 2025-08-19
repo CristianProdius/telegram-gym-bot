fe9f3c3e2a8450ecf4cfd0ca991f4068a25e5f8c
veacheslavv <144955981+veacheslavv@users.noreply.github.com>
2025-08-19 18:42:31 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,32 @@
 # src/main.py
 import asyncio
+import os
+from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from src.handlers.exercise_handlers import router
 
-import os
-from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from src.models.exercise import Base
+from src.data.seed_exercises import seed_exercises
 
 load_dotenv()
 TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+engine = create_engine("sqlite:///exercises.db")
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+if not session.query(Base.metadata.tables['exercises']).count():
+    seed_exercises(session)
 
 async def main():
     bot = Bot(token=TOKEN)
     storage = MemoryStorage()
     dp = Dispatcher(storage=storage)
+
     dp.include_router(router)
 
     print("Bot is running...")
@@ -21,4 +34,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
