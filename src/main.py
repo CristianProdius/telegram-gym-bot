87ee3bc6f791bfea21e3e7481a1de35a1ece0b77
veacheslavv <144955981+veacheslavv@users.noreply.github.com>
2025-08-19 18:37:08 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,24 @@
+# src/main.py
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from src.handlers.exercise_handlers import router
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+async def main():
+    bot = Bot(token=TOKEN)
+    storage = MemoryStorage()
+    dp = Dispatcher(storage=storage)
+    dp.include_router(router)
+
+    print("Bot is running...")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
