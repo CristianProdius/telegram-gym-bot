9a032af643295b8c93ab0f681c81271fc9846666
Slava Morari <slava.morari@gmail.com>
2025-08-24 17:19:53 +0300

    Add data validation for search_exercise command using pydantic (3 SP)

---
+++ src/models/exercise.py
@@ -0,0 +1,11 @@
+﻿from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Exercise(Base):
+    __tablename__ = 'exercises'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    category = Column(String)
+    primary_muscle = Column(String)
