a29a9388c511d64339d784ab42a2ed768096142e
Orm-Max <orm.maxim@gmail.com>
2025-09-11 13:51:48 +0300

    Описание изменений

--- main/feature/dev5_rest_timers/__init__.py
+++ main/feature/dev5_rest_timers/__init__.py
@@ -1,4 +1,9 @@
+from aiogram import Router
 from .handlers import router as handlers_router
 from .plan import router as plan_router
+
+router = Router()
+router.include_router(handlers_router)
+router.include_router(plan_router)
 
 __all__ = ["router"]
